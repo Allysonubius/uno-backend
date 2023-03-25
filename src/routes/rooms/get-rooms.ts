0c22ca257dac96902683bd994cf2f836bebff8d6
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2023-03-24 23:04:49 -0300

    Implemented list count room's

--- src/routes/rooms/get-rooms.ts
+++ src/routes/rooms/get-rooms.ts
@@ -9,18 +9,38 @@ import type { RequestWithUser } from '../../types/request-with-user';
 export const getRooms = async (req: RequestWithUser, res: Response) => {
   const { take, skip } = req.query;
 
-  res.json(
-    await prisma.room.findMany({
-      include: {
-        _count: {
-          select: { players: true },
-        },
+  const rooms = await prisma.room.findMany({
+    include: {
+      _count: {
+        select: { players: true },
       },
-      where: {
-        state: RoomState.WAITING_FOR_PLAYERS,
-      },
-      take: Number(take ?? 50),
-      skip: Number(skip ?? 0),
-    })
-  );
+    },
+    where: {
+      state: RoomState.WAITING_FOR_PLAYERS,
+    },
+    take: Number(take ?? 50),
+    skip: Number(skip ?? 0),
+  })
+
+  if (rooms.length === 0) {
+    return res.status(404).json({
+      message: "Nenhuma sala encontrada.",
+    });
+  }
+  
+  const totalRooms = await prisma.room.count({
+    where: {
+      state: RoomState.WAITING_FOR_PLAYERS,
+    },
+  });
+
+  const currentPage = Number(skip ?? 0) / Number(take ?? 50) + 1;
+  const totalPages = Math.ceil(totalRooms / Number(take ?? 50));
+
+  res.json({
+    totalRooms,
+    currentPage,
+    totalPages,
+    rooms,
+  })
 };
